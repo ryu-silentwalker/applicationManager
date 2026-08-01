79c704c9cfef3c27fbbb6783f3ce000eed0cfd98
ryu-silentwalker <ryusilentwalker@gmail.com>
2026-08-01 14:31:30 -0400

    Can get to application page but fails there. need to keep working on that.

--- lib/applicationBot.py
+++ lib/applicationBot.py
@@ -1,5 +1,51 @@
+import os
+from time import sleep
+from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class login():
-    def __init__(self):
+class linkedInBot():
+    def __init__(self,jobBlob):
         self.loginUri = "https://www.linkedin.com/login/?trk=guest_homepage-basic_nav-header-signin"
+        self.driver = webdriver.Firefox()
+        self.jobBlob = jobBlob
+        load_dotenv()
+        self.user = os.getenv("user")
+        self.passwd = os.getenv("passwd")
+
+    def login(self):
+        self.driver.get(self.loginUri)
+        emailBox = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID,"«R3jvukejj35655j6»")))
+        emailBox.clear()
+        emailBox.send_keys(self.user)
+        passwdBox = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID,"«Rlvukejj35655j6»")))
+        passwdBox.clear()
+        passwdBox.send_keys(self.passwd)
+        loginBox = self.driver.find_element(By.XPATH, "//span[text()='Sign in']").find_element(By.XPATH,"./..").find_element(By.XPATH,"./..")
+        self.driver.execute_script("arguments[0].click();", loginBox)
+
+    def gotoJob(self,obj):
+        self.driver.get(obj['link'])
+
+    def apply(self):
+        applyButton = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID,"link-external-medium"))).find_element(By.XPATH,"./..")
+        applyButton.click()
+        sleep(5)
+        try:
+            WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href*=*myworkday*]"))).click()
+        except:
+            pass
+        try:
+            WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href*=*autofillWithResume*]"))).click()
+        except:
+            pass
+
+    def autoApply(self):
+        self.login()
+        sleep(5)
+        for el in self.jobBlob:
+            self.gotoJob(self.jobBlob[el])
+            self.apply()
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import asyncio
 import json
+from time import sleep
 import lib.linkedInApiManager
+import lib.applicationBot
 
 queryObj = {
         "host": "www.linkedIn.com",
@@ -17,5 +19,16 @@ queryObj = {
         "verification": True,
         "under10": True
 }
-with open('test_out.json','w',encoding='utf-8') as out:
-    json.dump(lib.linkedInApiManager.query(queryObj).getJobBatch(),out, indent=2)
+
+obj = {
+  "title": "Cyber - Palo Alto SecOps - Senior Consultant",
+  "location": "Boston, MA",
+  "postedTime": "2026-08-01",
+  "link": "https://www.linkedin.com/jobs/view/cyber-palo-alto-secops-senior-consultant-at-deloitte-4447779096?position=10&pageNum=95&refId=NKk%2BCGdbKOJhaJlIJQQhrw%3D%3D&trackingId=RpYAUYG3Vfx55bhX%2BGG5zQ%3D%3D"
+}
+#with open('test_out.json','w',encoding='utf-8') as out:
+#    json.dump(lib.linkedInApiManager.query(queryObj).getJobBatch(),out, indent=2)
+
+
+bot = lib.applicationBot.linkedInBot(lib.linkedInApiManager.query(queryObj).getJobBatch())
+bot.autoApply()
